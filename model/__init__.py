975d61328616634d34a4d7b9caca1a1487bb2fad
MarlemER <Marlem123>
2026-05-13 13:02:37 -0400

    integrate final model training and prediction pipeline

---
+++ model/__init__.py
@@ -0,0 +1,6 @@
+"""
+Paquete de utilidades del modelo de ConcertDemandAI.
+
+Este archivo permite importar funciones desde la carpeta model/ como paquete:
+from model.predict import predict_event
+"""
